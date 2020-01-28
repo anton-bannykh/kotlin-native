ab5457302dc3dd4429a9c5cf659fac53be9966f5
Anton Bannykh <anton.bannykh@jetbrains.com>
2020-01-28 17:19:24 +0300

    override val extractedLocalClasses

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanBackendContext.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanBackendContext.kt
@@ -23,6 +23,7 @@ import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.builtins.konan.KonanBuiltIns
 import org.jetbrains.kotlin.ir.builders.IrBuilderWithScope
+import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrDeclaration
 import org.jetbrains.kotlin.ir.symbols.IrFunctionSymbol
 import org.jetbrains.kotlin.ir.symbols.IrSimpleFunctionSymbol
@@ -35,6 +36,9 @@ internal abstract class KonanBackendContext(val config: KonanConfig) : CommonBac
 
     override val transformedFunction: MutableMap<IrFunctionSymbol, IrSimpleFunctionSymbol>
         get() = TODO("not implemented")
+
+    override val extractedLocalClasses: MutableSet<IrClass> = hashSetOf()
+
     override val scriptMode: Boolean = false
 
     override val sharedVariablesManager by lazy {
